78cd30b2048145baac3e95b32698d304eec2d5bc
Gurudev Ilangovan <ilangurudev@gmail.com>
2017-11-05 22:28:50 -0600

    added hypothesis support for non-pdf types

--- db/schema.rb
+++ db/schema.rb
@@ -36,16 +36,8 @@ ActiveRecord::Schema.define(version: 20171105191706) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "project_files", force: :cascade do |t|
-    t.string   "file_name"
-    t.string   "description"
-    t.integer  "project_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "p_file"
-    t.string   "FileUpload"
-    t.string   "attachment"
-  end
+# Could not dump table "project_files" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
   create_table "project_users", force: :cascade do |t|
     t.integer  "project_id"
