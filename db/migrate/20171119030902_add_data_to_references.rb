7e02a03e54cf3887c75c889800d664bc40d3af0c
Rahul <rahul.26.saini@gmail.com>
2017-11-19 19:23:21 +0000

    Reference migration

--- db/migrate/20171119030902_add_data_to_references.rb
+++
@@ -1,8 +0,0 @@
-class AddDataToReferences < ActiveRecord::Migration
-  def change
-    add_column :references, :question_id, :integer
-    add_column :references, :answer_id, :integer
-    add_column :references, :file_id, :integer
-    add_column :references, :user_id, :integer
-  end
-end
