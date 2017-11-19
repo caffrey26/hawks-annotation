2168ef77646004faa70d995f7a26e79f74a38178
Rahul <rahul.26.saini@gmail.com>
2017-11-19 19:44:13 +0000

    anss-2

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 
 require 'csv'
-# require 'rails/all'
-require 'sprocket/railtie'
+require 'rails/all'
+# require 'sprocket/railtie'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
