015735937227fcc14ab5b33255eb8a326d7e94cc
Rahul <rahul.26.saini@gmail.com>
2017-11-26 00:16:53 +0000

    File attachment validation

--- app/models/project_file.rb
+++ app/models/project_file.rb
@@ -2,4 +2,6 @@ class ProjectFile < ActiveRecord::Base
     mount_uploader :attachment, AttachmentUploader
     belongs_to :project
     validates :file_name, presence: true,  uniqueness: { scope: :project_id }
+    validates_presence_of :attachment
+
 end
