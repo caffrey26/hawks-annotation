85d4fb608378cf47a86cb41c7ff780e1caaa6d6b
Rahul <rahul.26.saini@gmail.com>
2017-11-30 14:52:45 +0000

    query on downloads

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,11 +1,13 @@
 class DownloadsController < ApplicationController
   def index
-     @latest_answers = if Answer.group(:question_id).count.empty? then 0 else Answer.group(:question_id).pluck(:id).count end
+    # @latest_answers = if Answer.group(:question_id).count.empty? then 0 else Answer.group(:question_id).pluck(:id).count end
+     @latest_answers = Answer.where("created_at in (SELECT MAX(answers.created_at) FROM answers GROUP BY answers.question_id ORDER BY MAX(created_at) DESC)").count
      @all_answers = Answer.all.count
      @own_answers = Answer.where(user_id: current_user.id).count
 
     if params[:type] == "latest" then
-      @answers = Answer.group(:question_id)
+      # @answers = Answer.group(:question_id)
+      @answers = Answer.where("created_at in (SELECT MAX(answers.created_at) FROM answers GROUP BY answers.question_id ORDER BY MAX(created_at) DESC)")
     end
     if params[:type] == "all" then
       # answers = Answer.joins("JOIN OPTIONS, QUESTIONS on questions.id = options.question_id and questions.id=answers.question_id and questions.q_type ='O' and answers.answer_text = options.id and answers.project_id = " + params[:project_id]).distinct.pluck("questions.title", "options.option_text")
